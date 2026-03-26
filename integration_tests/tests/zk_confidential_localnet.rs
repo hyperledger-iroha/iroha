a02b8f88694d3b2c4f9cf30c7720be69180e0788
Makoto Takemiya <takemiya@soramitsu.co.jp>
2026-03-26 15:48:04 +0400

    fix test

--- integration_tests/tests/zk_confidential_localnet.rs
+++ integration_tests/tests/zk_confidential_localnet.rs
@@ -3266,6 +3266,7 @@ fn pressure_submitter_clients_applies_short_timeouts() {
         transaction_ttl: ttl,
         transaction_status_timeout: ttl,
         connect_queue_root: default_connect_queue_root(),
+        soracloud_http_witness_file: None,
         sorafs_alias_cache: AliasCachePolicy::new(ttl, ttl, ttl, ttl, ttl, ttl, ttl, ttl),
         sorafs_anonymity_policy: AnonymityPolicy::default(),
         sorafs_rollout_phase: iroha_config::parameters::actual::SorafsRolloutPhase::default(),
