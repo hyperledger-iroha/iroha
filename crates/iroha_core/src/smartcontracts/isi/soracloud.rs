12b986ae7055d810f4ab5c844888cc581ef0fbcc
Makoto Takemiya <t@soramitsu.co.jp>
2026-04-15 04:16:31 -0700

    fix compile

--- crates/iroha_core/src/smartcontracts/isi/soracloud.rs
+++ crates/iroha_core/src/smartcontracts/isi/soracloud.rs
@@ -14046,6 +14046,8 @@ mod tests {
                         kernel_image_path: "/inrou/x86_64/vmlinux".to_string(),
                         rootfs_image_path: "/inrou/x86_64/rootfs.ext4".to_string(),
                         initrd_image_path: None,
+                        distribution: Default::default(),
+                        published_artifact: None,
                     },
                 ),
                 (
@@ -14054,6 +14056,8 @@ mod tests {
                         kernel_image_path: "/inrou/aarch64/vmlinux".to_string(),
                         rootfs_image_path: "/inrou/aarch64/rootfs.ext4".to_string(),
                         initrd_image_path: None,
+                        distribution: Default::default(),
+                        published_artifact: None,
                     },
                 ),
             ]),
